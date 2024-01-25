673a8dbcceee3cb0cca57a47c4c2b92de07e6f01
SEOK7 <cokecake77@naver.com>
2024-01-25 21:56:54 +0900

    Breath_Effect 코드 추가

---
+++ Form2.Designer.cs
@@ -0,0 +1,110 @@
+﻿namespace WinFormsApp1
+{
+    partial class Form2
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            components = new System.ComponentModel.Container();
+            System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(Form2));
+            pictureBox1 = new System.Windows.Forms.PictureBox();
+            Breath_Effect = new System.Windows.Forms.Timer(components);
+            label1 = new System.Windows.Forms.Label();
+            value1 = new System.Windows.Forms.Label();
+            button1 = new System.Windows.Forms.Button();
+            ((System.ComponentModel.ISupportInitialize)pictureBox1).BeginInit();
+            SuspendLayout();
+            // 
+            // pictureBox1
+            // 
+            pictureBox1.Image = (System.Drawing.Image)resources.GetObject("pictureBox1.Image");
+            pictureBox1.Location = new System.Drawing.Point(104, 57);
+            pictureBox1.Name = "pictureBox1";
+            pictureBox1.Size = new System.Drawing.Size(225, 225);
+            pictureBox1.SizeMode = System.Windows.Forms.PictureBoxSizeMode.StretchImage;
+            pictureBox1.TabIndex = 0;
+            pictureBox1.TabStop = false;
+            // 
+            // Breath_Effect
+            // 
+            Breath_Effect.Interval = 18;
+            Breath_Effect.Tick += Breath_Effect_Tick;
+            // 
+            // label1
+            // 
+            label1.AutoSize = true;
+            label1.Font = new System.Drawing.Font("맑은 고딕", 15F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point);
+            label1.Location = new System.Drawing.Point(468, 119);
+            label1.Name = "label1";
+            label1.Size = new System.Drawing.Size(80, 35);
+            label1.TabIndex = 1;
+            label1.Text = "size : ";
+            // 
+            // value1
+            // 
+            value1.AutoSize = true;
+            value1.Font = new System.Drawing.Font("맑은 고딕", 15F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point);
+            value1.Location = new System.Drawing.Point(561, 119);
+            value1.Name = "value1";
+            value1.Size = new System.Drawing.Size(82, 35);
+            value1.TabIndex = 1;
+            value1.Text = "label1";
+            // 
+            // button1
+            // 
+            button1.Location = new System.Drawing.Point(178, 350);
+            button1.Name = "button1";
+            button1.Size = new System.Drawing.Size(138, 50);
+            button1.TabIndex = 2;
+            button1.Text = "숨쉬기";
+            button1.UseVisualStyleBackColor = true;
+            button1.Click += button1_Click;
+            // 
+            // Form2
+            // 
+            AutoScaleDimensions = new System.Drawing.SizeF(9F, 20F);
+            AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            ClientSize = new System.Drawing.Size(800, 450);
+            Controls.Add(button1);
+            Controls.Add(value1);
+            Controls.Add(label1);
+            Controls.Add(pictureBox1);
+            Name = "Form2";
+            Text = "Form2";
+            ((System.ComponentModel.ISupportInitialize)pictureBox1).EndInit();
+            ResumeLayout(false);
+            PerformLayout();
+        }
+
+        #endregion
+
+        private System.Windows.Forms.PictureBox pictureBox1;
+        private System.Windows.Forms.Timer Breath_Effect;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Label value1;
+        private System.Windows.Forms.Button button1;
+    }
+}
---
+++ Form2.cs
@@ -0,0 +1,41 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace WinFormsApp1
+{
+    public partial class Form2 : Form
+    {
+        public Form2()
+        {
+            InitializeComponent();
+            Breath_Effect.Enabled = false;
+        }
+        static int Second_count = 0;
+
+        private void Breath_Effect_Tick(object sender, EventArgs e)
+        {
+            Second_count++;
+            double size = 3 * Math.Sin(Second_count * 0.1); // 정수 3 부분으로 크기를 조절하고, 0.1 부분으로 주기를 조절합니다.
+            int size_Width = pictureBox1.Size.Width;
+            int size_Height = pictureBox1.Size.Height; // 사진의 크기값들을 불러옵니다.
+
+
+            Size tosize = new Size(size_Height + (int)size, size_Width + (int)size); // 사진의 크기값에 계산값을 더합니다.
+            pictureBox1.Size = tosize; // 사진에 위 계산값의 위치값을 부여합니다.
+
+            value1.Text = size.ToString();
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            Breath_Effect.Enabled = true;
+        }
+    }
+}
